221690f7155d639575e0488e23f0fba62715f061
Hannah Hahm <hannahhahm@gmail.com>
2019-05-29 09:25:44 -0500

    practicing applying php to the blog

---
+++ include/header.php
@@ -0,0 +1,27 @@
+<?php
+
+    function echoHeader($pageTitle){
+        echo "
+        <html>
+            <head>
+                
+            </head>
+        
+            <body>
+        
+        
+            
+        
+            <div class='header'>
+            <h1>Hannah Hahm</h1>
+            </div>
+        
+            <div class=headerlinks>
+        
+                        <a href='http://localhost:8888/website.html'><h2>Home</h2></a>
+                        <a href='http://localhost:8888/aboutme.html'><h2>About Me</h2></a>
+                        <a href='http://localhost:8888/link2.html'><h2>Experiences</h2></a>
+                        <a href='http://localhost:8888/link3.html'><h2>Contact</h2></a>
+             </div>
+            ";
+    }
