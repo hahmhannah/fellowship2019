339d07c1ed26cb93dba505548222b9dda3331f9a
Hannah Hahm <hannahhahm@gmail.com>
2019-06-04 10:07:08 -0500

    making the comment page

--- viewpost.php
+++ viewpost.php
@@ -24,4 +24,18 @@ echo "
 
     var_dump($result);
 */
+?>
+
+<h4>Comments</h4>
+
+<form action='' method='post'>
+    Name:
+    <input type='text' name='author' /><br />
+
+    Comment:
+    <textarea name='content' rows='4' cols='30'></textarea><br />
+
+        <input type='submit' name='newComment' value='Submit Comment' />
+
+</form>
     
