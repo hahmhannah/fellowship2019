acb5d694e9ca5e9ff455d1a25702926d03c52a04
Hannah Hahm <hannahhahm@gmail.com>
2019-06-05 12:46:32 -0500

    php function exercises

---
+++ daysinamonth.php
@@ -0,0 +1,25 @@
+<?php /*
+    function daysInAMonth($monthId){
+        $monthId = array('1' => '31', '2' => '28', '3' => '31', '4' => '30', '5' => '31', '6' => '30', '7' => '31', '8' => '31', '9' => '30', '10' => '31', '11' => '31', '12' => '31');
+       
+        return $numDays;
+      
+    }
+
+
+    echo daysInAMonth('6');
+*/
+
+  
+    function daysInAMonth($monthId){
+        $numDays = something;
+        foreach ($monthId as $numDays){
+        ['1' => '31', '2' => '28', '3' => '31', '4' => '30', '5' => '31', '6' => '30', '7' => '31', '8' => '31', '9' => '30', '10' => '31', '11' => '31', '12' => '31'];
+       
+       
+        }
+    return $numDays;
+    }
+
+
+    echo daysInAMonth('6');
---
+++ leetspeak.php
@@ -0,0 +1,45 @@
+<?php
+    function leetspeak($inputWord){
+        $leetSpokenWord = '';
+        $wordAsArray = str_split($inputWord);
+        foreach($wordAsArray as $letter){
+            if ($letter === 'a'){
+                $leetId = 4;
+                $leetSpokenWord = $leetSpokenWord.$leetId;
+            }
+            else if($letter === 'e'){
+                $leetId = 3;
+                $leetSpokenWord = $leetSpokenWord.$leetId;
+            }
+            else if ($letter === 'g'){
+                $leetId = 9;
+                $leetSpokenWord = $leetSpokenWord.$leetId;
+            }   
+            else if ($letter === 'l'){
+                $leetId = 1;
+                $leetSpokenWord = $leetSpokenWord.$leetId;
+            }
+            else if ($letter === '0'){
+                $leetId = 0;
+                $leetSpokenWord = $leetSpokenWord.$leetId;
+            }
+            else if ($letter === 's'){
+                $leetId = 5;
+                $leetSpokenWord = $leetSpokenWord.$leetId;
+            }
+            else if ($letter === 't'){
+                $leetId = 7;
+                $leetSpokenWord = $leetSpokenWord.$leetId;
+            }
+
+            else{
+               $leetSpokenWord = $leetSpokenWord.$letter;
+            }
+
+        
+       
+    }
+    echo $leetSpokenWord;
+}
+  leetspeak('apple');
+
