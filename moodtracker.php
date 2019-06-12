9f63a24bdaec3874aa14ee947cdf373fd987d810
Hannah Hahm <hannahhahm@gmail.com>
2019-06-12 08:59:39 -0500

    design stuff

--- moodtracker.php
+++ moodtracker.php
@@ -2,7 +2,15 @@
     <head><link href="moodtracker.css" rel="stylesheet" type="text/css"></head>
     
     <body>
-        <div class='homepage'></div>
+        <div class='homePage'>
+            <h1 class='welcomeSign'>Welcome</h1>
+        </div>
+
+        <div class='homePageDiv2'>
+            <h2 class='aboutUs'>About this website</h2>
+        </div>  
+
+
 
     </body>
 
