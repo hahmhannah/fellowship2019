1d91a99014b577c67073282c67ec6ee7d51e313b
Hannah Hahm <hannahhahm@gmail.com>
2019-06-13 16:43:03 -0500

    Navigation Bar

--- moodtracker.php
+++ moodtracker.php
@@ -2,17 +2,42 @@
     <head><link href="moodtracker.css" rel="stylesheet" type="text/css"></head>
     
     <body>
-        <div class='navBar'></div>
+        <div class='navBar'>
+            <ul class='navBarParent'>
+                <div class='navBarChild1'>
+                    <li class='navAbout'><a href='#'>about</a></li>
+                    <li class='navHowItWorks'><a href='#'>how it works</a></li>
+                    <li class='navResources'><a href='#'>resources</a></li>
+                    <li class='navExtra'><a href='#'>extra</a></li>
+                </div> 
+                <div class='navBarChild2'>   
+                    <li class='navLogin'><a href='#'>login</a></li>
+                    <li class='navSignUp'><a href='#'>sign up</a></li>
+                </div>
+            </ul>       
+        </div>
+
         <div class='homePage'>
             <h1 class='frontPageAmbiance'>ambiance</h1>
                 <p class='frontPageMiniText'>a mood tracker</p>
         </div>
 
-       <!-- <div class='homePageDiv2'>
-            <h2 class='aboutUs'>About this website</h2>
-        </div>   -->
+       <div class='homePageDivAbout'>
+            <h2 class='homePageAboutDivTitle'>about this website</h2>
+        </div>   
 
+        <div class='homePageDivResources'>
+            <h2 class='homePageResourcesDivTitle'>resources</h2>
+        </div>  
 
+        <div class='homePageDivHowItWorks'>
+            <h2 class='homePageHowItWorksDivTitle'>how it works</h2>
+        </div>  
+
+        <div class='homePageDivExtra'>
+            <h2 class='homePageExtraTitle'>extra space</h2>
+                <p>maybe footer/add login button</p>
+        </div> 
 
     </body>
 
