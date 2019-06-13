170a6517fce29af00edaa948ecc19e25963a8dc5
Hannah Hahm <hannahhahm@gmail.com>
2019-06-13 14:05:10 -0500

    Background image on a div-fixing bottom cut off problem

--- moodtracker.php
+++ moodtracker.php
@@ -2,13 +2,15 @@
     <head><link href="moodtracker.css" rel="stylesheet" type="text/css"></head>
     
     <body>
+        <div class='navBar'></div>
         <div class='homePage'>
-            <h1 class='welcomeSign'>Welcome</h1>
+            <h1 class='frontPageAmbiance'>ambiance</h1>
+                <p class='frontPageMiniText'>a mood tracker</p>
         </div>
 
-        <div class='homePageDiv2'>
+       <!-- <div class='homePageDiv2'>
             <h2 class='aboutUs'>About this website</h2>
-        </div>  
+        </div>   -->
 
 
 
