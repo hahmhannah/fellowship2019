d5ace7a41c77c251fc0ae8d4dc70caa5c15809fe
Hannah Hahm <hannahhahm@gmail.com>
2019-06-27 10:05:10 -0500

    starting to make form

--- resume.php
+++ resume.php
@@ -1,2 +1,21 @@
 <html>
     <head><link href='resume.css' rel='stylesheet' type='text/css'></head>
+
+    <form action='' method='post'>   
+        Name:
+        <input type='text' name='name' /><br />
+        
+        Description
+        <input type='text' name='description' /><br />
+
+        Position:
+        <select name='category'>
+                <option value='Produce'>Fruits and veggies</option>
+                <option value='Drink'>Soda and other drinks</option>
+                <option value='Frozen'>Frozen food aisle</option>
+                <option value='Dairy'>The dairy section</option>
+        </select>
+
+        <br/><br/>
+        <input type='submit' name='newInventoryItem' value='Submit your application' />
+</form>
