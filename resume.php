6fda856abd750de655ee9bc39d7e15ae9103ad2e
Hannah Hahm <hannahhahm@gmail.com>
2019-06-28 09:22:47 -0500

    Update resume.php

--- resume.php
+++ resume.php
@@ -1,21 +1,31 @@
-<html>
+<html> <!--profile page info --> 
     <head><link href='resume.css' rel='stylesheet' type='text/css'></head>
 
-    <form action='' method='post'>   
-        Name:
-        <input type='text' name='name' /><br />
-        
-        Description
-        <input type='text' name='description' /><br />
+    <form action='' method='post'>  
+        ID Number:
+        <input type='text' name='age' /><br />
 
-        Position:
-        <select name='category'>
-                <option value='Produce'>Fruits and veggies</option>
-                <option value='Drink'>Soda and other drinks</option>
-                <option value='Frozen'>Frozen food aisle</option>
-                <option value='Dairy'>The dairy section</option>
-        </select>
+        Age:
+        <input type='text' name='age' /><br />
+        
+        Occupation:
+        <input type='text' name='occupation' /><br />
+
+       <!-- Type of caffeinated beverage do you normally drink:
+            <select name='category'>
+                <option value='Produce'>Coffee</option>
+                <option value='Drink'>Tea</option>
+                <option value='Drink'>Other</option>
+            </select><br /> -->
+
+        Cups of caffeinated beverage you drink a day:
+        <input type='text' name='liquidAmount' /><br />
+
+        Occupation
+        <input type='text' name='occupation' /><br />
+
+        
 
         <br/><br/>
-        <input type='submit' name='newInventoryItem' value='Submit your application' />
+        <input type='submit' name='' value='Submit' />
 </form>
