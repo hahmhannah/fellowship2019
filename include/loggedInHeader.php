68cc80a719c2a79728d4c6a54f3045029ed13c0e
Hannah Hahm <hannahhahm@gmail.com>
2019-07-11 16:00:03 -0500

    changing file names

--- include/loggedInHeader.php
+++ include/loggedInHeader.php
@@ -11,7 +11,7 @@
                     <div class='navBar'>
                         <ul class='navBarParent'>
                             <div class='navBarChild1'>
-                                <li class='navAbout'><a href='/moodtracker.php'>home</a></li>
+                                <li class='navAbout'><a href='/index.php'>home</a></li>
                                 <li class='navAbout'><a href='#anchor1'>about</a></li>
                                 <li class='navHowItWorks'><a href='#anchor2'>how it works</a></li>
                                 <li class='navResources'><a href='#anchor3'>resources</a></li>
