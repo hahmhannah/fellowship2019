9f4a7c4f16366df9a2fd80db9f16095037b2786f
Hannah Hahm <hannahhahm@gmail.com>
2019-07-12 09:17:25 -0500

    New header

--- signUpConfirmation.php
+++ signUpConfirmation.php
@@ -1,5 +1,5 @@
 <?php
     include('config/init.php');
-    echologgedInHeader("Sign Up Confirmed");
+    echoTrackerHeader("Sign Up Confirmed");
 
     echo "Thank you for signing up. Click <a href='/trackerLogin.php'> here </a> to login. " ;
