ea829b4f9c2b44f58e95d673b42ae57bcfc53c36
Hannah Hahm <hannahhahm@gmail.com>
2019-07-20 15:08:41 -0500

    Css

---
+++ personalWebsite.php
@@ -0,0 +1,28 @@
+<?php
+include('config/init.php');
+
+?>
+
+<html>
+    <head>
+        <link href='personalWebsite.css' rel='stylesheet' type='text/css'>           
+    </head>
+
+
+    <body>
+        
+        <div class='mainPagePictureDiv'>
+        <div class='mainNavBarDiv'></div>
+        </div>
+        <div class='getToKnowMeDiv'>Get To Know Me</div>
+        <div class='educationDiv'>Education</div>
+        <div class='thingsIveDoneDiv'>Things I've Done</div>
+        <div class='myThoughtsDiv'>My Thoughts</div>
+        <div class='footerContactDiv'>Contact Me</div>
+    </body>
+
+
+
+
+
+</html>
