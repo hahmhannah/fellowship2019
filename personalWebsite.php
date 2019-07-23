d1d2a76787ece05bac714405d744e1eed076792c
Hannah Hahm <hannahhahm@gmail.com>
2019-07-23 09:02:40 -0500

    Education Div

--- personalWebsite.php
+++ personalWebsite.php
@@ -1,6 +1,6 @@
 <?php
 include('config/init.php');
-
+//By Source, Fair use, https://en.wikipedia.org/w/index.php?curid=30998682
 ?>
 
 <html>
@@ -11,14 +11,70 @@ include('config/init.php');
 
     <body>
         
+    
         <div class='mainPagePictureDiv'>
-        <div class='mainNavBarDiv'></div>
+            <div class='mainNavBarDiv'>nav bar</div>
+            <div class='mainPageIntroduction'>Hi, I'm Hannah Hahm </div>
+            <div class='mainPageIntroductionSmallText'>student and aspiring researcher </div>
         </div>
-        <div class='getToKnowMeDiv'>Get To Know Me</div>
-        <div class='educationDiv'>Education</div>
-        <div class='thingsIveDoneDiv'>Things I've Done</div>
-        <div class='myThoughtsDiv'>My Thoughts</div>
-        <div class='footerContactDiv'>Contact Me</div>
+
+        <div class='getToKnowMeDiv'>
+            <h2 class='divTitles'>Get To Know Me</h2>
+        </div>
+
+        <div class='educationDiv'>
+            <h2 class='divTitles'>Education</h2>
+            <hr class='lineUnderneathDivTitles'>
+
+                <div class='educationRowWashu'>
+                    <div class='educationRowPictureWashu'>
+                        <img src='washuPicture.png' class='washuLogo'>
+                    </div>
+                    <div class='educationRowWords'>
+                        <h4>Washington University in St. Louis</h4>
+                            <p>August 2018-May 2022</p>
+                                <p>Some words about WashU</p>
+                    </div>
+                </div>
+                <div class='educationRowBradshaw'>
+                    <div class='educationRowPictureBradshaw'>
+                        <img src='bcsLogo.png' class='bcsLogo'>
+                    </div>
+                    <div class='educationRowWords'>
+                        <h4>Bradshaw Christian High School</h4>
+                            <p>August 2014-June 2018</p>
+                                <p>Some words about Bradshaw</p>
+                    </div>
+                </div>
+        </div>
+
+       <!-- <div class='educationDivContainer'>
+            <div class='washuLogo'>
+                <img src='washuLogo.jpg' class='washulogo'> 
+            </div>
+            <div class='educationWords'>
+                <p class='educationWordTitles'>Washington University in St. Louis </p> 
+                    <p>August 2018-June 2022</p>
+                    <p>Words about college</p><br>
+
+                <p class='educationWordTitles'>Bradshaw Christian High School<p>
+                    <p>August 2014-June 2018</p>
+                    <p>Words about high school </p>
+            </div>
+            
+        </div> -->
+
+        <div class='thingsIveDoneDiv'>
+            <h2 class='divTitles'>Things I've done</h2>
+        </div>
+
+        <div class='myThoughtsDiv'>
+            <h2 class='divTitles'>My Thoughts</h2>
+        </div>
+
+        <div class='footerContactDiv'>
+            <h2 class='divTitles'>Contact</h2>
+        </div>
     </body>
 
 
