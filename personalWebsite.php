202037f7b849fcf7cd88b06535304d2a92495f17
Hannah Hahm <hannahhahm@gmail.com>
2019-07-29 16:00:55 -0500

    Create personalWebsite.php

---
+++ personalWebsite.php
@@ -0,0 +1,182 @@
+<?php
+include('config/init.php');
+//By Source, Fair use, https://en.wikipedia.org/w/index.php?curid=30998682
+?>
+
+<html>
+    <head>
+        <link href='personalWebsite.css' rel='stylesheet' type='text/css'>           
+    </head>
+
+
+    <body>
+
+        <div class='navBarDiv'>
+            <ul class='navBarParent'>
+                 <div class='navBarChild1'>
+                    <li class='navName'><a href='#' class='headerLinks'>Hannah Hahm</a></li>
+                </div> 
+                <div class='navBarChild2'>   
+                    <li class='navAbout'><a href='#' class='headerLinks'>About</a></li>
+                    <li class='navExperiences'><a href='#' class='headerLinks'>Experiences</a></li>
+                    <li class='navThoughts'><a href='#' class='headerLinks'>Thoughts</a></li>
+                    <li class='navContact'><a href='#' class='headerLinks'>Contact</a></li>
+                </div>
+            </ul>  
+        </div>
+        
+    
+        <div class='mainPagePictureDiv'>
+            
+            <div class='mainPageIntroduction'>Hi, I'm Hannah Hahm </div>
+            <div class='mainPageIntroductionSmallText'>student and aspiring researcher </div>
+        </div>
+
+        <div class='getToKnowMeDiv'>
+            <h2 class='divTitles'>Get To Know Me</h2>
+            <hr class='lineUnderneathDivTitles'>
+<!--
+            <div class='educationRowBradshaw'>
+                    <div class='educationRowPictureBradshaw'>
+                        <img src='bcsLogo.png' class='bcsLogo'>
+                    </div>
+            </div>          --> 
+            
+            <div class='educationRowWashu'>
+                    <div class='educationRowPictureWashu'>
+                        <img src='washuPicture.png' class='washuLogo'>
+                    </div>
+                    <div class='educationRowWords'>
+                        <ul>
+                            <li>I'm a rising sophomore at Washington University in St. Louis pursuing a degree in Neuroscience </li> <br>
+                            <li>Some classes I've taken that I liked: Psych 3195 Abnormal Child Psychology, BIOL 2960 Principles of Biology I, Chem 111 General Chemistry I, Chem 112 General Chemistry II, Chem 151 General Chemistry Lab I </li><br>
+                            <li>Hobbies: running, coding, watching Marvel movies, trying to bake </li><br>
+                            <li>I'm interested in medicine and research and hope to become a doctor someday</li>
+                    </div>
+                </div>
+        </div>
+
+       <!-- <div class='educationDiv'>
+            <h2 class='divTitles'>Education</h2>
+            <hr class='lineUnderneathDivTitles'>
+
+                <div class='educationRowWashu'>
+                    <div class='educationRowPictureWashu'>
+                        <img src='washuPicture.png' class='washuLogo'>
+                    </div>
+                    <div class='educationRowWords'>
+                        <h4>Washington University in St. Louis</h4> 
+                            <p>August 2018-May 2022</p>
+                            <br>
+                                <p>I am currently a rising sophomore pursuing a degree in Neuroscience. </p>
+                    </div>
+                </div>
+                 <div class='educationRowBradshaw'>
+                    <div class='educationRowPictureBradshaw'>
+                        <img src='bcsLogo.png' class='bcsLogo'>
+                    </div>
+                   <div class='educationRowWords'>
+                        <h4>Bradshaw Christian High School</h4>
+                            <p>August 2014-June 2018</p>
+                                <p>Some words about Bradshaw</p>
+                    </div>
+                </div> -->
+        </div>
+
+    
+        <div class='thingsIveDoneDiv'>
+            <h2 class='divTitles'>Things I've done</h2>
+            <hr class='lineUnderneathDivTitles'>
+                <div class='thingsIveDoneLACRM'>
+                    <div class='thingsIveDoneLACRMWords'>
+                        <h3 class='wordDivTitles'>Summer 2019 Coding Fellow @ Less Annoying CRM</h3>
+                            <br>
+                        <p>Learned how to code with HTML, CSS, PHP, Javascript, and MySQL at Less Annoying CRM</p> <br>
+                        <p>Created an e-diary website that allows you to track daily mood, stress, and habits, and provides a place to 
+                            store your thoughts and reflect.  </p> <br>
+                        <p>Created this personal website. </p>
+                    </div>
+                    <div class='thingsIveDoneLACRMPicture'>
+                        <img src='pictureLACRM.png' class='logoLACRM'>
+                    </div>
+                </div>
+                <div class='thingsIveDoneResearch'>
+                    <div class='thingsIveDoneResearchWords'>
+                        <h3 class='wordDivTitles'>Gereau Pain Neurobiology Lab Research Assistant </h3>
+                            <br>
+                        <p>Trained in lab procedures such as DNA precipitation, PCR, and gel electrophoresis. </p> <br>
+                        <p>In the process being trained in mouse blood perfusions. </p> <br>
+                        <p>Involved in mouse data entry and organization. </p> <br>
+                    </div>
+                    <div class='thingsIveDoneResearchPicture'>
+                        <img src='pictureResearch.jpg' class='medicineLogo'>
+                    </div>
+                </div>
+                <div class='thingsIveDoneKSA'>
+                    <div class='thingsIveDoneKSAWords'>
+                        <h3 class='wordDivTitles'>WUSTL Korean Student Association </h3>
+                            <br>
+                        <p>Freshman representative in KSA, a club to share Korean culture with others</p><br>
+                        <p>Promoted KSA events to peers as freshman representative</p><br>
+                        <p>Helped plan, promote, and put on 2019 Spirit of Korea, annual KSA spring festival </p><br>
+                        <p>Performed in Open Dance 2019 for KSA</p>
+                    </div>
+                    <div class='thingsIveDoneKSAPicture'>
+                        <img src='pictureKSA.png' class='logoKSA'>
+                    </div>
+                </div>
+        </div>
+        <div class='skillsDiv'>
+            <h2 class='divTitles'>Skills</h2>
+            <hr class='lineUnderneathDivTitles'>
+                <div class='skill1'>
+                    <div class='skill1Picture'></div>
+                    <div class='skill1Words'></div>
+                </div>
+                <div class='skill2'>
+                    <div class='skill1Picture'></div>
+                    <div class='skill1Words'></div>
+                </div>
+                <div class='skill3'>
+                    <div class='skill1Picture'></div>
+                    <div class='skill1Words'></div>
+                </div>
+        </div>
+        <div class='myThoughtsDiv'>
+            <h2 class='divTitles'>My Thoughts</h2>
+            <hr class='lineUnderneathDivTitles'>
+                <div class='myThoughtsLACRMExperience'>
+                    <div class='myThoughtsLACRMExperiencePicture'>
+                        <img src='pictureLACRM.png' class='logoLACRM'>
+                    </div>
+                    <div class='myThoughtsLACRMExperienceWords'>
+                        <h3 class='wordDivTitles'>my lacrm experience</h3>
+                            <br>
+                        <p>blah blah </p>
+                    </div>
+                </div>
+                <div class='myThoughtsGoals'>
+                    <div class='myThoughtsGoalsPicture'>
+                        <img src='pictureResearch.jpg' class='medicineLogo'>
+                    </div>
+                    <div class='myThoughtsGoalsWords'>
+                        <h3 class='wordDivTitles'>Gereau Lab Research Assistant </h3>
+                            <br>
+                        <p>Pain neurobiology lab</p>
+                    </div>
+                </div>
+                
+        </div>
+
+        <div class='footerContactDiv'>
+            <h2 class='divTitles'>Contact</h2>
+            <hr class='lineUnderneathDivTitles'>
+                
+        </div>
+    </body>
+
+
+
+
+
+</html>
